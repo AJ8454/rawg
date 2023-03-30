7047ea66ea23ba441c8e3fad17939ca2c0e32880
AJ8454 <ajaypangare8454@gmail.com>
2023-03-30 11:51:56 +0530

    Genres done

---
+++ lib/src/app/features/trending/presentation/bloc/trending_state.dart
@@ -0,0 +1,9 @@
+part of 'trending_bloc.dart';
+
+abstract class TrendingState extends Equatable {
+  const TrendingState();  
+
+  @override
+  List<Object> get props => [];
+}
+class TrendingInitial extends TrendingState {}
